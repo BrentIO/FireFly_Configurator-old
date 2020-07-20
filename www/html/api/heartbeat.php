6f040e44d7900a93df0dee53197652cbf6626a68
Hendricks, Brent Andrew <brent.hendricks@p5software.com>
2020-07-19 20:40:15 -0400

    Initial release

---
+++ www/html/api/heartbeat.php
@@ -0,0 +1,31 @@
+<?php
+    ini_set('display_errors', 'On');
+
+    require_once('simpleRest.php');
+    require_once('database.php');
+
+    $database = new database();
+    $simpleRest = new simpleRest();
+
+    $procCall = $database->callProcedure("CALL getHeartbeat(@response)");
+
+    //Check to make sure the procedure was successful
+    if($procCall == true)
+    {
+        $simpleRest->setHttpHeaders(200);
+
+        //Get the response back from the getHeartbeat SP
+        $heartbeat = $database->query("SELECT 'ok' AS 'status', @response AS timeUTC;");
+        
+        //Output the time and status
+        print($heartbeat);
+
+    }else{
+
+        $simpleRest->setHttpHeaders(500);
+
+        //The procedure call had a failure (wrong name or no rows in the settings table?), so return that the system is unhealthy
+        print('{"status":"failed"}');
+        
+    }
+?>
