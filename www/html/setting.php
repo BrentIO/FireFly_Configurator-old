b6b04729d557afef58ba7bc82186914223320819
Hendricks, Brent Andrew <brent.hendricks@p5software.com>
2020-08-15 07:57:52 -0400

    Initial commit

---
+++ www/html/setting.php
@@ -0,0 +1,253 @@
+<?php
+
+    require_once('./api/getConfig.php');
+    require_once('common.php');
+
+    //Validate the user is logged in
+    checkLogin();
+
+    $pageName = "setting";
+    $url = "http://" . $_SERVER['SERVER_ADDR'] . "/api/setting";
+
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>FireFly Configurator - Settings</title>
+        <link rel="stylesheet" href="bootstrap.min.css">
+        <link rel="stylesheet" href="style.css">
+        <script src="jquery.min.js"></script>
+        <script src="bootstrap.min.js"></script>
+        <script src="jquery.toaster.js"></script>
+        <script>        
+
+            $(document).ready(function(){
+
+                class setting {
+                    id = null;
+                    value = null;
+                }
+            
+                //Load the data on page load
+                loadTableData();
+
+
+                $('#modalEditItem').on('show.bs.modal', function (event) {
+                    /*************************************************
+                    **  Handle the edit item modal being requested  **
+                    *************************************************/
+
+                    //Retrieve the data associated to the button press event
+                    var button = $(event.relatedTarget);
+                    var operation = button.data('operation');
+
+                    //Clear existing input
+                    destroyEditData();
+
+                    var editItemForm = document.editItem;
+
+                    if(operation == "add"){
+
+                        //Set the modal title
+                        document.getElementById("operation").innerHTML= "Add New";
+
+                    }
+
+                    //If editing, retrieve the data from the API
+                    if(operation == "edit"){
+
+                        //Set the modal title
+                        document.getElementById("operation").innerHTML= "Edit Existing";
+
+                        $.ajax({
+
+                            beforeSend: function(request) {
+                                request.setRequestHeader("x-api-key", "<?php print(getConfig("x-api-key")); ?>");
+                            },
+
+                            type: 'GET',
+                            url: "<?php print($url);?>" + '/' + button.data('uniqueid'),
+
+                            success: function(data) {
+
+                                //Populate the field elements with the data returned by the API
+                                  editItemForm.elements["uniqueId"].value = data['id']; 
+                                editItemForm.elements["value"].value = data['value'];                        
+                            },
+
+                            error: function(data){
+                                $.toaster({ priority :'danger', title :'Failed', message : data['status'] + ' ' + data['statusText']});
+                            },
+                        });
+                    }
+                });
+
+
+                $(document).on('click', '#buttonSave', function(event){
+
+                    var editItemForm = document.editItem;
+
+                    elementName = editItemForm.elements["name"];
+
+                    var item = new setting();
+
+                    item.id = editItemForm.elements["uniqueId"].value;
+                    item.value = editItemForm.elements["value"].value;
+
+                    if(item.id == ""){
+                        item.id = null;
+                    }
+
+                    //Send the item to the API
+                    editItem(item);
+
+                    //Hide the modal
+                    $("#modalEditItem").modal("hide");
+
+                });
+
+
+                function destroyEditData(){
+                    /*************************************************
+                    **  Destroys the data on the edit form          **
+                    *************************************************/
+
+                    //Get the form inputs
+                    var editItemForm = document.editItem;
+
+                    //For each input
+                    for(var i = 0; i < editItemForm.length; i++){
+                        
+                        //Clear the data
+                        editItemForm[i].value = null;
+                    }
+
+                };
+
+
+                function editItem(item){
+                    /*************************************************
+                    **  Adds new or edits existing entries based on **
+                    **      the ID passed in                        **
+                    *************************************************/
+
+                    if(item.id == null){
+                        var method="POST";
+                        var url = "<?php print($url);?>";
+                    }else{
+                        var method="PATCH";
+                        var url = "<?php print($url);?>" + '/' + item.id;
+                    }
+
+                    $.ajax({
+
+                        beforeSend: function(request) {
+                            request.setRequestHeader("x-api-key", "<?php print(getConfig("x-api-key")); ?>");
+                        },
+                        data: JSON.stringify(item),
+                        type: method,
+                        url: url,
+
+                        success: function(data) {
+                            $.toaster({ priority :'success', title :'Edit', message : 'Successful'});
+                        },
+
+                        error: function(data){
+                            $.toaster({ priority :'danger', title :'Failed', message : data['status'] + ' ' + data['statusText']});
+                        },
+
+                        complete: function(){
+                            loadTableData();
+                        }
+
+                    });
+
+                }
+
+
+                function loadTableData(){
+                    /*************************************************
+                    **  Loads the data from the API into the data   **
+                    **      table for viewing and editing           **
+                    *************************************************/
+
+                    //Delete the existing contents of the table
+                    $('.dynamic').remove();
+
+                    //Load the table
+                    $.ajax({
+                        beforeSend: function(request) {
+                            request.setRequestHeader("x-api-key", "<?php print(getConfig("x-api-key")); ?>");
+                        },
+                        url: "<?php print($url);?>",
+
+                        success: function(data) {
+                            $.each(data, function(i){
+                                trHTML = "<tr class=\"dynamic\">"
+                                            + "<td>" + data[i].displayName + "</td>"
+                                            + "<td>" + data[i].value + "</td>"
+                                             + "<td><button class=\"btn btn-default\" data-toggle=\"modal\" data-target=\"#modalEditItem\" data-backdrop=\"static\" data-operation=\"edit\" data-uniqueid=\"" + data[i].id + "\">Edit</button>"
+                                            + "</td>"
+                                        +"</tr>"
+                                $('#dataTable').append(trHTML);
+                            });
+                        },
+
+                        fail: function(data){
+                            $.toaster({ priority :'danger', title :'Failed', message : data['status'] + ' ' + data['statusText']});
+                        }
+                    });
+                };
+            
+            });
+
+        </script>
+    </head>
+    <body>
+
+<?php include "menu.php"?>
+        
+        <div class="content">
+        <div id="pageName">
+            <div id="pageTitle">Settings</div>
+        </div>
+
+        <table id="dataTable">
+            <tbody>
+            <tr>
+                <th>Setting</th>
+                <th>Value</th>
+                <th>Operations</th>
+            </tr>
+            </tbody>
+        </table>
+
+
+        <!-- Edit Item Modal -->
+        <div class="modal fade" id="modalEditItem" role="dialog">
+            <div class="modal-dialog">
+
+                <!-- Modal content-->
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal">&times;</button>
+                        <h4 class="modal-title"><div id="operation"></div></h4>
+                    </div>
+                    <div class="modal-body" name="form">
+                        <form name="editItem">
+                            <input type="hidden" id="uniqueId">
+                            <label for="value">Value:</label>
+                            <input type="text" id="value" size="50">
+                        </form>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                        <button type="button" class="btn btn-primary" id="buttonSave">Save</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        </div>
+    </body>
+</html>
