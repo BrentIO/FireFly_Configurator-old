41e9257ddaff191049d44533ed27175475aab729
Hendricks, Brent Andrew <brent.hendricks@p5software.com>
2020-08-14 21:59:11 -0400

    Changed to local references

--- www/html/breaker.php
+++ www/html/breaker.php
@@ -13,10 +13,10 @@
 <html>
     <head>
         <title>FireFly Configurator - Breakers</title>
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+        <link rel="stylesheet" href="bootstrap.min.css">
         <link rel="stylesheet" href="style.css">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+        <script src="jquery.min.js"></script>
+        <script src="bootstrap.min.js"></script>
         <script src="jquery.toaster.js"></script>
         <script>        
 
