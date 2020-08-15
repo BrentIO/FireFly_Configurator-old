25cfc27b639af59552f805caa1d1630facbbd64b
Hendricks, Brent Andrew <brent.hendricks@p5software.com>
2020-08-14 22:59:58 -0400

    Fixed active height

--- www/html/menu.php
+++ www/html/menu.php
@@ -8,9 +8,9 @@
             <a <?php if($pageName == "action"){?>class="active"<?php };?>href="action.php">Actions</a>
             <a <?php if($pageName == "firmware"){?>class="active"<?php };?>href="firmware.php">Firmware</a>
             <a <?php if($pageName == "setting"){?>class="active"<?php };?>href="setting.php">Settings</a>
-            <a <?php if($pageName == "buttonColor"){?>class="active"<?php };?>href="buttonColor.php" class="child">Button Colors</a>
-            <a <?php if($pageName == "brightnessName"){?>class="active"<?php };?>href="brightnessName.php" class="child">Brightness Names</a>
-            <a <?php if($pageName == "map"){?>class="active"<?php };?>href="map.php">Connectivity Map</a>
+            <div class="child"><a <?php if($pageName == "buttonColor"){?>class="active"<?php };?>href="buttonColor.php">Button Colors</a></div>
+            <div class="child"><a <?php if($pageName == "brightnessName"){?>class="active"<?php };?>href="brightnessName.php">Brightness Names</a></div>
+            <a <?php if($pageName == "connectivityMap"){?>class="active"<?php };?>href="map.php">Connectivity Map</a>
             <a href="logout.php">Logout</a>
         </div>
       
